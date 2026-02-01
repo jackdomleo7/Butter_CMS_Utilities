3813a7001a0f77db52374b415e18cbaba1bb3523
Jack Domleo <jackdomleo7@gmail.com>
2026-02-01 15:18:36 +0000

    infra: Move site to buttercmsutilities.jackdomleo.dev subdomain

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Butter_CMS_Utilities/',
   plugins: [
     vue(),
     vueDevTools(),
